edc09f5cdeb6639da05f44dfb9680f350de99a45
MarcosGit2020 <marrubdia@alu.edu.gva.es>
2023-12-20 19:21:27 +0100

    0.2

--- src/main/java/es/ieslavereda/Password2/Password.java
+++ src/main/java/es/ieslavereda/Password2/Password.java
@@ -1,7 +1,34 @@
 package es.ieslavereda.Password2;
 
 public class Password {
-    private int longitud;
     private static final int longitudPorDefecto = 10;
+    private int longitud;
     private String contarseña;
+
+    public Password() {
+        this.longitud = longitudPorDefecto;
+        this.contarseña = generarPassword(this.longitud);
+    }
+
+    public Password(int longitud) {
+        this.longitud = longitud;
+        this.contarseña = generarPassword(this.longitud);
+    }
+    public String generarPassword(int longitud){
+        String contraseña = "";
+        for (int i = 0; i < longitud; i++) {
+
+        }
+        return contraseña;
+    }
+    public int getLongitud(){
+        return longitud;
+    }
+    public String getContarseña(){
+        return contarseña;
+    }
+    public void setLongitud(int longitud){
+        this.longitud = longitud;
+        this.contarseña = generarPassword(this.longitud);
+    }
 }
